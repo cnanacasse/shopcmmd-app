3b7751b9a7beeb11b95e2ac07281587aaaac4006
Clare Nanacasse <clare@christinemeyermd.com>
2026-02-23 19:21:10 -0500

    Add merch page

---
+++ app/merch/page.tsx
@@ -0,0 +1,15 @@
+export default function Merch() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      flexDirection: "column",
+      fontFamily: "Arial"
+    }}>
+      <h1>Welcome to the CMMD Staff Merch Store</h1>
+      <p>This is your internal merch dashboard.</p>
+    </div>
+  );
+}
